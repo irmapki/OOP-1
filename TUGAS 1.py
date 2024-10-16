90cf7dcad3230463de69636baa0519f6aa78d40b
irmapki <152676953+irmapki@users.noreply.github.com>
2024-10-16 09:44:38 +0700

    Add files via upload

---
+++ TUGAS 1.py
@@ -0,0 +1,12 @@
+class mobil:
+  name = ""
+  roda = 0
+  wiper = 0
+
+mobilmewah = mobil()
+
+mobilmewah.roda = 10
+mobilmewah.wiper = 3
+mobilmewah.name = "mobil super mewah hyundai"
+
+print(f"Name : {mobilmewah.name}, roda : {mobilmewah.roda}, wiper : {mobilmewah.wiper}")
